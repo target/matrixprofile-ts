928af0545d7bb75b1adccfd2041c539f6b460e0b
Andrew Van Benschoten <andrew.vanbenschoten@target.com>
2018-09-21 14:22:23 -0500

    Final edits to setup dot py and README

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="matrixprofile-ts",
-    version="0.0.1",
+    version="0.0.2",
     author="Andrew Van Benschoten",
     author_email="andrew.vanbenschoten@target.com",
-    description="An Open Source Time Series Library For Motif Discovery",
+    description="An Open Source Python Time Series Library For Motif Discovery using Matrix Profile",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/target/matrixprofile-ts",
