7c3d70792d46a47199118d25b6ec243a53e743f3
Manuel Barkhau <mbarkhau@gmail.com>
2019-01-04 18:22:24 +0100

    add py2 compat boilerplate

--- matrixprofile/distanceProfile.py
+++ matrixprofile/distanceProfile.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+range = getattr(__builtins__, 'xrange', range)
+# end of py2 compatability boilerplate
+
 from .utils import *
 import numpy as np
 
