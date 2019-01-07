03490e5a6ad87a296fa0cb42c77f7b470800a581
mbarkhau <mbarkhau@gmail.com>
2019-01-07 03:15:45 +0100

    Feature Python 2 Compatibility (#19)
    * basic py2 compat
    
    github issue #5
    
    * don't shadow builtin 'iter'
    
    * add py2 compat boilerplate

--- matrixprofile/matrixProfile.py
+++ matrixprofile/matrixProfile.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+range = getattr(__builtins__, 'xrange', range)
+# end of py2 compatability boilerplate
+
 from . import distanceProfile
 from . import order
 from .utils import mass, movmeanstd
@@ -49,9 +58,9 @@ def _matrixProfile_sampling(tsA,m,orderClass,distanceProfileFunction,tsB=None,sa
     #Define max numbers of iterations to sample
     iters = (len(tsA)-m+1)*sampling
 
-    iter = 0
+    iter_val = 0
 
-    while iter < iters:
+    while iter_val < iters:
         (distanceProfile,querySegmentsID) = distanceProfileFunction(tsA,idx,m,tsB)
 
         #Check which of the indices have found a new minimum
@@ -64,7 +73,7 @@ def _matrixProfile_sampling(tsA,m,orderClass,distanceProfileFunction,tsB=None,sa
         mp = np.minimum(mp,distanceProfile)
         idx = order.next()
 
-        iter += 1
+        iter_val += 1
     return (mp,mpIndex)
 
 
--- matrixprofile/utils.py
+++ matrixprofile/utils.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+range = getattr(__builtins__, 'xrange', range)
+# end of py2 compatability boilerplate
+
 import numpy as np
 import numpy.fft as fft
 
